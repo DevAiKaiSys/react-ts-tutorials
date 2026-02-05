b42f7733f43e2c7123b51b3686117e271ad13b27
DevAiKaiSys <dev.aikaisys@gmail.com>
2026-02-05 23:32:38 +0700

    Showcase

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Hero from "./sections/Hero.tsx";
+import ShowcaseSection from "./sections/ShowcaseSection.tsx";
 
 const App = () => {
     return (
         <>
-            <Hero/>
+            <Hero />
+            <ShowcaseSection />
         </>
     )
 }
