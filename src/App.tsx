5d436252209f18fb4ff3464645187ae72d7fb95c
DevAiKaiSys <dev.aikaisys@gmail.com>
2026-02-06 23:20:36 +0700

    Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import NavBar from "./components/NavBar.tsx";
 import Hero from "./sections/Hero.tsx";
 import ShowcaseSection from "./sections/ShowcaseSection.tsx";
 
 const App = () => {
     return (
         <>
+            <NavBar />
             <Hero />
             <ShowcaseSection />
         </>
